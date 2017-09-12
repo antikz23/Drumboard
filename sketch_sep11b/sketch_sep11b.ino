45773633f5b5608a56d8e1e9c4f9cf9c5d95196a
antikz23 <jslizzz30@gmail.com>
2017-09-12 09:14:03 -0400

    Add files via upload

---
+++ sketch_sep11b/sketch_sep11b.ino
@@ -0,0 +1,64 @@
+int x;
+int y;
+int z;
+/*
+  Menu driven control of a sound board over UART.
+  Commands for playing by # or by name (full 11-char name)
+  Hard reset and List files (when not playing audio)
+  Vol + and - (only when not playing audio)
+  Pause, unpause, quit playing (when playing audio)
+  Current play time, and bytes remaining & total bytes (when playing audio)
+
+  Connect UG to ground to have the sound board boot into UART mode
+*/
+
+#include <SoftwareSerial.h>
+#include "Adafruit_Soundboard.h"
+
+
+// Choose any two pins that can be used with SoftwareSerial to RX & TX
+#define SFX_TX 5
+#define SFX_RX 6
+
+// Connect to the RST pin on the Sound Board
+#define SFX_RST 4
+
+// You can also monitor the ACT pin for when audio is playing!
+
+// we'll be using software serial
+SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
+
+// pass the software serial to Adafruit_soundboard, the second
+// argument is the debug port (not used really) and the third
+// arg is the reset pin
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
+// can also try hardware serial with
+//Adafruit_Soundboard sfx = Adafruit_Soundboard(&Serial1, NULL, SFX_RST);
+
+void setup()
+{
+  ss.begin(9600);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+}
+
+void loop()
+{
+  x = digitalRead(3);
+  y = digitalRead(2);
+  z = digitalRead(1);
+  if (x == LOW)
+  {
+    digitalWrite(7, !  sfx.playTrack(7) );
+  }
+  if (y == LOW)
+  {
+    digitalWrite(5, !  sfx.playTrack(5) );
+  }
+  if (z == LOW)
+  {
+    digitalWrite(4, !  sfx.playTrack(4) );
+  }
+
+}

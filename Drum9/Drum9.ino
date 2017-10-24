c1e70de42f8c37852c236c2637e1682e5d260bc9
antikz23 <jslizzz30@gmail.com>
2017-10-24 10:11:28 -0400

    Add files via upload

---
+++ Drum9/Drum9.ino
@@ -0,0 +1,48 @@
+
+#include <SoftwareSerial.h>
+#include "Adafruit_Soundboard.h"
+#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+#include <Adafruit_GFX.h>
+#include "Adafruit_LEDBackpack.h"
+
+// Choose any two pins that can be used with SoftwareSerial to RX & TX
+#define SFX_TX    5
+#define SFX_RX    6
+#define SFX_RST   4
+
+#define sound_delay 50
+byte val = 0;
+
+// we'll be using software serial
+SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
+
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
+
+void setup()
+{
+  touch = cap.touched();
+  if (touch != 0)
+  {
+    //Serial.print(touch);
+
+    if (touch == 1)
+    {
+      Serial.print(1);
+      sfx.playTrack(1);
+      delay(sound_delay);
+    }
+    if (touch == 2)
+    {
+      Serial.print(2);
+      sfx.playTrack(2);
+      delay(sound_delay);
+    }
+    if (touch == 4)
+    {
+      Serial.print(4);
+      sfx.playTrack(3);
+      delay(sound_delay);
+    }
+  }
+  delay(200);
+}
---
+++ TouchSounds1/TouchSounds1.ino
@@ -0,0 +1,57 @@
+#include <SoftwareSerial.h>
+#include "Adafruit_Soundboard.h"
+#include <Adafruit_Soundboard.h>
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+
+#define SFX_TX    5
+#define SFX_RX    6
+#define SFX_RST   4
+
+#define sound_delay 50
+byte val = 0;
+
+SoftwareSerial ss = SoftwareSerial(SFX_TX, SFX_RX);
+Adafruit_Soundboard sfx = Adafruit_Soundboard(&ss, NULL, SFX_RST);
+
+int touch;
+Adafruit_MPR121 cap = Adafruit_MPR121();
+
+
+void setup()
+{
+  ss.begin(9600);
+  sfx.reset();
+  Serial.begin(9600);
+  cap.begin(0x5A);
+}
+
+void loop()
+{
+  touch = cap.touched();
+  if (touch != 0)
+  {
+    //Serial.print(touch);
+
+    if (touch == 1)
+    {
+      Serial.print(1);
+      sfx.playTrack(1);
+      delay(sound_delay);
+    }
+    if (touch == 2)
+    {
+      Serial.print(2);
+      sfx.playTrack(2);
+      delay(sound_delay);
+    }
+    if (touch == 4)
+    {
+      Serial.print(4);
+      sfx.playTrack(3);
+      delay(sound_delay);
+    }
+  }
+  delay(200);
+}
+
